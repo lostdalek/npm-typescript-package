2ffcd03c1a9ca1f89719f817178970015709e850
Florian Blouet <florian.blouet@implicity.fr>
2017-10-29 13:11:16 +0100

    import base configuration

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {NpmTypescriptPackage} from './interface';
+import Entrypoint = NpmTypescriptPackage.Entrypoint;
+
+export const entryPoint: Entrypoint = function () {
+    return '';
+};
+export * from './interface';
+
+
---
+++ src/interface.ts
@@ -0,0 +1,7 @@
+namespace NpmTypescriptPackage {
+    export interface Entrypoint {
+        (): string;
+    }
+
+}
+export {NpmTypescriptPackage};
