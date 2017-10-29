6aed7f70f6cd7b59eab4d78fc7e5bb988117d436
Florian Blouet <florian.blouet@implicity.fr>
2017-10-29 16:01:20 +0100

    add a version getter

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,9 +1,13 @@
-import {entryPoint} from '../src/index';
 import * as NpmTypescriptPackage from '../src/index';
+
 declare const jest, describe, it, expect;
 
 describe('Typescript npm package Boilerplate', () => {
     it('should expose an entryPoint method', () => {
         expect(NpmTypescriptPackage.entryPoint).toBeInstanceOf(Function)
+    });
+
+    it('should return the actual version', () => {
+        expect(NpmTypescriptPackage.getVersion()).toEqual('0.0.1')
     });
 });
